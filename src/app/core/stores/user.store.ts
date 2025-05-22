fb75f3bb2bbd87e281e2dc4f55d55edc8af1b0f4
jordi from virtual machine <jordi@gmail.com>
2025-05-22 09:37:16 +0200

    comentario sobre store

--- src/app/core/stores/user.store.ts
+++ src/app/core/stores/user.store.ts
@@ -19,3 +19,16 @@ function loadPayLoadFromLocalStorage(): any | null {
     const data = localStorage.getItem('user_payload');
     return data ? JSON.parse(data) : null;
 }
+
+// ✅ En resumen:
+// Un store en Angular 19 no es una clase concreta ni una librería! Es un patrón basado en signals.
+// Se usa para mantener y exponer estado reactivo compartido sin añadir dependencias externas.
+
+// Ejemplos de dependencias externas para gestionar estado en Angular:
+//
+// 1. NgRx: Inspirado en Redux. Potente pero con mucho boilerplate.
+// 2. Akita: Gestión de estado basada en entidades. Más sencilla que NgRx. Orientada a proyectos complejos.
+// 3. NgXs: Más simple y declarativo. Usa decoradores y clases. Menos verboso que NgRx.
+//
+// 📌 Todas estas librerías externas se instalan con npm.
+// Gracias a signals (Angular 16+), puedes evitar estas dependencias para muchos casos de uso comunes.
